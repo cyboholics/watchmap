3b41b63f00aa4d9f43d3654604bbe2beaa689e98
Sudip Mondal <sudipmondal.2002@rediffmail.com>
2023-03-12 01:45:41 +0530

    add registration endpoint

--- monitor/pages/api/register.ts
+++ monitor/pages/api/register.ts
@@ -2,13 +2,32 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
 import prisma from "../../libs/prisma";
 
-export default function handler(
+export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     const {name, ip, port}: {
         name: string,
         ip: string,
-        port: number
+        port: string
     } = req.body
+
+    await prisma.serviceInstance.create({
+        data: {
+            ip: ip,
+            port: port,
+            // @ts-ignore
+            service: {
+                connectOrCreate: {
+                    where: {
+                        name: name
+                    }, create: {
+                        name: name
+                    }
+                }
+            }
+        }
+    })
+
+    res.status(201).end()
 }
