b9352621824fb59f7ae567b100abe429589976e6
Sudip Mondal <sudipmondal.2002@rediffmail.com>
2023-03-13 00:10:55 +0530

    remove previous endpoints

--- monitor/pages/api/record.ts
+++ monitor/pages/api/record.ts
@@ -3,20 +3,22 @@ import type {NextApiRequest, NextApiResponse} from 'next'
 import {Collection, Event, Script} from "postman-collection";
 import * as fs from "fs";
 import newman from "newman";
+import prisma from "../../libs/prisma";
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     const collection = new Collection(JSON.parse(req.body))
+    console.log(process.env.PROJECT_HOST)
     const preRequestScript = new Script({
         exec: [
-            `pm.sendRequest(${process.env.PROJECT_HOST}/api/start-request?current_request=\`\$\{pm.request.url.getPath()\}\`);`
+            `pm.sendRequest(\`${process.env.PROJECT_HOST}/api/start-request?current_request=\${pm.request.url.getPath()}\`);`
         ]
     })
     const testScript = new Script({
         exec: [
-            `pm.sendRequest(${process.env.PROJECT_HOST}/api/end-request?current_request=\`\$\{pm.request.url.getPath()\}\`);`
+            `pm.sendRequest(\`${process.env.PROJECT_HOST}/api/end-request?current_request=\${pm.request.url.getPath()}\`);`
         ]
     })
 
@@ -34,9 +36,11 @@ export default async function handler(
     collection.events.add(testEvent)
 
     fs.writeFileSync('collection.json', JSON.stringify(collection.toJSON()))
+    await prisma.endpointService.deleteMany({})
+    await prisma.endpoint.deleteMany({})
     newman.run({
         collection: 'collection.json',
-    });
+    })
 
     res.end()
 }
