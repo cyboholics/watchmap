cfe1add3b733b631fb3db59767ce012e33d4112b
Sudip Mondal <sudipmondal.2002@rediffmail.com>
2023-03-13 01:32:07 +0530

    add homepage

--- monitor/pages/index.tsx
+++ monitor/pages/index.tsx
@@ -1,46 +1,91 @@
-import {Button, Container} from "@mui/material";
 import React from "react";
-import {CollectionTree} from "../components/CollectionTree";
+import {Box, Container, Grid, Switch, Typography} from "@mui/material";
+
+type EndpointService = {
+    allEndPoints: string[],
+    allServices: string[],
+    mapping: {
+        endpoint: string,
+        services: string[]
+    }[]
+}
 
 export default function Home() {
-    const [collection, setCollection] = React.useState();
-    return (
-        <Container>
-            <Button
-                variant="contained"
-                component="label"
-            >
-                Upload File
-                <input
-                    type="file"
-                    onChange={(e) => {
-                        if (!e.target.files) return
-                        const file = e.target.files[0]
-                        if (!file) return
-                        // read json file
-                        const reader = new FileReader()
-                        reader.onload = (e) => {
-                            const text = e.target?.result
-                            if (typeof text === 'string') {
-                                const data = JSON.parse(text)
-                                setCollection(data)
-                            }
-                        }
-                        reader.readAsText(file)
-                    }}
-                    accept="application/json"
-                    hidden
-                />
-            </Button>
-            {collection && <Button onClick={async ()=>{
-                await fetch('/api/record', {
-                    method: 'POST',
-                    body: JSON.stringify(collection)
-                })
+    const [data, setData] = React.useState<EndpointService>({
+        allEndPoints: [],
+        allServices: [],
+        mapping: []
+    })
+    const [failedServices, setFailedServices] = React.useState<string[]>([])
+    const {allEndPoints, allServices, mapping} = data
+
+    const isEndpointDown = (endpoint: string) => {
+        const services = mapping.find((m) => {return m.endpoint === endpoint})?.services
+        if(!services) return false
+        for (const service of services) {
+            if (failedServices.includes(service)) {
+                return true
+            }
+        }
+        return false
+    }
+
+    React.useEffect(() => {
+        fetch('/api/endpoint-services').then(async (res) => {
+            setData(await res.json())
+        })
+    }, [])
+    return <>
+        <Typography textAlign={"center"} variant={"h3"}>Monitor</Typography>
+
+        <Container sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+        }}>
+            <Container sx={{
+                width: '70%'
             }}>
-                Start Recording
-            </Button>}
-            {collection && <CollectionTree collectionData={collection}/>}
+                <Grid container={true} spacing={2}>
+                    {
+                        allServices.map((service) => {
+                            return <Grid key={service} item={true} xs={12} sm={6} md={4}>
+                                <Box sx={{
+                                    backgroundColor: '#99ff99',
+                                    padding: '10px',
+                                    borderRadius: '10px',
+                                }}>
+                                    <Typography>{service}</Typography>
+                                    <Switch
+                                        checked={!failedServices.includes(service)}
+                                        onChange={(e) => {
+                                            if (e.target.checked) {
+                                                setFailedServices(failedServices.filter((s) => s !== service))
+                                            } else {
+                                                setFailedServices([...failedServices, service])
+                                            }
+                                        }}
+                                    />
+                                </Box>
+                            </Grid>
+                        })
+                    }
+                </Grid>
+            </Container>
+            <Container sx={{
+                width: '30%',
+            }}>
+                {
+                    allEndPoints.map((endpoint) => {
+                        return <Box key={endpoint}>
+                            <Typography sx={{
+                                    color: isEndpointDown(endpoint) ? 'red' : 'green'
+                                }}>{endpoint}</Typography>
+                        </Box>
+                    })
+                }
+            </Container>
         </Container>
-    )
+    </>
 }
+
