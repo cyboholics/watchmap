9d6c8cb481411139c26d7b9926ce7b4ce3998bcd
Sudip Mondal <sudipmondal.2002@rediffmail.com>
2023-03-21 21:33:32 +0530

    fix async await

--- servers/express-1/src/index.ts
+++ servers/express-1/src/index.ts
@@ -4,26 +4,29 @@ import axios from 'axios';
 
 const app = express();
 
-console.log(process.env)
+const setup = async () => {
 
-app.use(watchmap())
+    app.use(await watchmap())
 
-app.get('/express1-test1', (req, res) => {
-    console.log("express1-test1 called")
-    res.send('Hello World From Test1!');
-})
+    app.get('/express1-test1', (req, res) => {
+        console.log("express1-test1 called")
+        res.send('Hello World From Test1!');
+    })
 
-app.get('/express1-test2', async (req, res) => {
-    console.log("express1-test2 called")
-    await axios.get('http://watchmap-express-2:3002/express2-test1')
-    res.send('Hello World From Test2!');
-});
+    app.get('/express1-test2', async (req, res) => {
+        console.log("express1-test2 called")
+        await axios.get('http://watchmap-express-2:3002/express2-test1')
+        res.send('Hello World From Test2!');
+    });
 
-app.get('/express1-test3', (req, res) => {
-    console.log("express1-test3 called")
-    res.send('Hello World From Test3!');
-});
+    app.get('/express1-test3', (req, res) => {
+        console.log("express1-test3 called")
+        res.send('Hello World From Test3!');
+    });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}`);
-})
+    app.listen(process.env.PORT, () => {
+        console.log(`Example app listening on port ${process.env.PORT}`);
+    })
+}
+
+setup()
